f9007ea931e8480506f4c2571adade997c38c332
FrazaAdityaWiguna <bigdreamnums@gmail.com>
2021-06-29 22:49:31 +0700

    Dynamic Routes (part 2 - getStaticPaths)

--- pages/dream/[id].js
+++ pages/dream/[id].js
@@ -1,6 +1,22 @@
 import React from "react";
 import Layout from "../../comps/Layout";
 
+export const getStaticPaths = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = res.json();
+
+  const paths = data.map((dream) => {
+    return {
+      params: { id: dream.id.toString() },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
 const Details = () => {
   return (
     <Layout>

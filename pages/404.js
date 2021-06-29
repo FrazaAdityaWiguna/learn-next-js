1b4b4420bdb83cf8c00852c74a64422b6d9a603d
FrazaAdityaWiguna <bigdreamnums@gmail.com>
2021-06-29 11:54:15 +0700

    Redirecting Users

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
 import Link from "next/link";
 
 const NotFound = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/");
+    }, 3000);
+  });
+
   return (
     <div className='not-found'>
       <h1>Ooooops...</h1>

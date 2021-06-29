3a4afabe5d30aa8eb3b6d6f9d50423476c806237
FrazaAdityaWiguna <bigdreamnums@gmail.com>
2021-06-29 10:43:48 +0700

    Pages & Routes

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const about = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint quae dolor laborum nemo ullam veniam animi ab voluptas labore laboriosam.</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam nisi a eligendi ullam necessitatibus beatae exercitationem blanditiis deserunt incidunt ipsum enim at voluptatem consequuntur ratione provident sed, eos ab illo.
+      </p>
+    </div>
+  );
+};
+
+export default about;

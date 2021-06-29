ec2a97239b6432b8d48b92567591879a9de6ff70
FrazaAdityaWiguna <bigdreamnums@gmail.com>
2021-06-29 19:47:52 +0700

    Fetching Data (getStaticProps)

--- pages/dream/index.js
+++ pages/dream/index.js
@@ -1,11 +1,27 @@
 import React from "react";
+import styles from "../../styles/Dream.module.css";
 import Layout from "../../comps/Layout";
-import Navbar from "../../comps/Navbar";
 
-const Dream = () => {
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  return {
+    props: { dreams: data },
+  };
+};
+
+const Dream = ({ dreams }) => {
   return (
     <Layout title='Member'>
       <h1>BIG DREAM member</h1>
+      {dreams.map((dream) => (
+        <div key={dream.id}>
+          <a className={styles.single}>
+            <h3>{dream.name}</h3>
+          </a>
+        </div>
+      ))}
     </Layout>
   );
 };
